1f58f19563bca6c9c0fc6fb36be9719fc62db583
andreystepanov <code@andrey.co>
2020-06-15 20:54:05 +0300

    Fixed reports import

--- dist/index.js
+++ dist/index.js
@@ -3,19 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "reports", {
-  enumerable: true,
-  get: function () {
-    return _reports2.default;
-  }
-});
 exports.default = void 0;
 
-var _reports2 = _interopRequireDefault(require("./reports"));
+var _reports = _interopRequireDefault(require("./reports"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _default = {
-  reports
+  reports: _reports.default
 };
 exports.default = _default;
